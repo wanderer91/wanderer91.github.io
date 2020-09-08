67ad1a16a78f21e0db966e5fd7b3c442ed54944c
medved_v_ushanke <getingjagare@gmail.com>
2020-09-08 20:13:13 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const miniCssWebpackPlugin = require('mini-css-extract-plugin');
+const uglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
+const optimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const cleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: './src/js/app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    {
+                        loader: miniCssWebpackPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                            name: '[name].css'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: '../img',
+                            emitFile: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff2?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: '../fonts'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new miniCssWebpackPlugin({
+            filename: '[name].css',
+            chunkFilename: '[name].css'
+        }),
+    ],
+    optimization: {
+        minimizer: [
+            new cleanWebpackPlugin(),
+            new uglifyJsWebpackPlugin({
+                uglifyOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+            }),
+            new optimizeCSSAssetsPlugin({
+                cssProcessorPluginOptions: {
+                    preset: ['default', { discardComments: { removeAll: true } }],
+                },
+            })
+        ]
+    }
+};
