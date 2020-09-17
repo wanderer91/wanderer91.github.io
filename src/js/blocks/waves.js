2d6f2a30395f8997f04b4099dc266b4e5c6d862a
medved_v_ushanke <getingjagare@gmail.com>
2020-09-17 21:55:13 +0300

    waves

--- src/js/blocks/waves.js
+++ src/js/blocks/waves.js
@@ -13,58 +13,105 @@ function generateBorderRadiuses(waveWidth) {
 
 }
 
+let wavesStyle = '';
+let count, counter, isMobile, waveWrapperWidth;
+
+function generateWaveStyles({id, waveWidth, waveHeight, rotateDir}) {
+
+    return `.waves__item#${id} {` +
+        `width: ${waveHeight}px;` +
+        `height: ${0.9 * waveWidth}px;` +
+        `z-index: ${count - counter};` +
+        `right: ${-waveWrapperWidth / count + waveWrapperWidth / count * counter}px;` +
+        `bottom: ${(isMobile ? -0.75 : -0.85) * waveHeight}px;` +
+        `border-radius: ${generateBorderRadiuses(waveWidth)};` +
+        `transform: rotate(0deg);` +
+        `animation-name: wave_${id};` +
+        `animation-duration: ${animationDuration};` +
+        `animation-timing-function: linear;` +
+        `animation-iteration-count: infinite;` +
+        `} @keyframes wave_${id} {` +
+        `0% {transform: rotate(0deg)}` +
+        `50% {transform: rotate(${rotateDir * 180}deg)}` +
+        `100% {transform: rotate(${rotateDir * 360}deg)}}`;
+
+}
+
+function initWaves() {
+
+    count = counter = 0;
+    isMobile = window.innerWidth < 640;
+
+    waveWrapperWidth = waveWrapper.offsetWidth;
+
+    waveWrapper.innerHTML = '';
+
+    const wavesData = [
+        {perspective: 'front', count: waveWrapper.dataset.frontWavesCount},
+        {perspective: 'back', count: waveWrapper.dataset.backWavesCount}
+    ];
+
+    wavesData.forEach(function (data) {
+
+        const countData = JSON.parse(data.count);
+
+        if (countData.constructor.name === 'Object') {
+
+            for (let bp in countData) {
+
+                if (window.innerWidth >= parseFloat(bp)) {
+                    data.count = parseInt(countData[bp]);
+                    count += parseInt(countData[bp]);
+                } else {
+                    break;
+                }
+
+            }
+
+        } else {
+            data.count = parseInt(countData);
+            count += parseInt(countData);
+        }
+
+    });
+
+    wavesData.forEach(function (data, i) {
+
+        for (let j = 0; j < data.count; j++) {
+
+            const wave = document.createElement('div');
+            const rotateDir = Math.random() < 0.5 ? -1 : 1;
+            const id = `${data.perspective}${i + 1}-wave${j + 1}`;
+
+            wave.className = `waves__item waves__item_${data.perspective}`;
+            wave.setAttribute('id', id);
+            waveWrapper.appendChild(wave);
+
+            const waveWidth = waveWrapperWidth * wavePercWidth / 100;
+            const waveHeight = 0.9 * waveWidth;
+
+            wavesStyle += generateWaveStyles({
+                id,
+                waveWidth,
+                waveHeight,
+                rotateDir
+            });
+
+            counter ++;
+
+        }
+
+    });
+
+    waveWrapper.innerHTML += `<style>${wavesStyle}</style>`;
+
+}
+
 const waveWrapper = document.querySelector('.waves');
-waveWrapper.parentNode.style.padding = '0 !important;';
 
 const wavePercWidth = parseInt(waveWrapper.dataset.wavePercWidth);
-const waveWrapperWidth = waveWrapper.offsetWidth;
-const waveWrapperHeight = waveWrapper.offsetHeight;
-const frontWavesCount = parseInt(waveWrapper.dataset.frontWavesCount);
-const backWavesCount = parseInt(waveWrapper.dataset.backWavesCount);
 const animationDuration = waveWrapper.dataset.animationDuration;
 
-let wavesStyle = '';
-let count = frontWavesCount + backWavesCount;
-let counter = 0;
+initWaves();
 
-[
-    {perspective: 'front', count: frontWavesCount},
-    {perspective: 'back', count: backWavesCount}
-].forEach(function (data, i) {
-
-    for (let j = 0; j < data.count; j++) {
-
-        const wave = document.createElement('div');
-        const rotateDir = Math.random() < 0.5 ? -1 : 1;
-        wave.className = `waves__item waves__item_${data.perspective}`;
-        wave.setAttribute('id', `perspective${i + 1}-wave${j + 1}`);
-        waveWrapper.appendChild(wave);
-
-        const waveWidth = waveWrapperWidth * wavePercWidth / 100;
-        const waveHeight = 0.9 * waveWidth;
-
-        wavesStyle += `.waves__item#perspective${i + 1}-wave${j + 1} {` +
-            `width: ${waveHeight}px;` +
-            `height: ${0.9 * waveWidth}px;` +
-            `z-index: ${count - counter};` +
-            `right: ${-waveWidth / 3 + waveWidth / 3 * counter}px;` +
-            `bottom: ${-0.85 * waveHeight}px;` +
-            `border-radius: ${generateBorderRadiuses(waveWidth)};` +
-            `transform: rotate(0deg);` +
-            `animation-name: wave${i + 1}-${j + 1};` +
-            `animation-duration: ${animationDuration};` +
-            `animation-timing-function: linear;` +
-            `animation-iteration-count: infinite;` +
-            `}`;
-
-        wavesStyle += `@keyframes wave${i + 1}-${j + 1} {` +
-            `0% {transform: rotate(0deg)}` +
-            `50% {transform: rotate(${rotateDir * 180}deg)}` +
-            `100% {transform: rotate(${rotateDir * 360}deg)}}`;
-
-        counter ++;
-    }
-
-});
-
-waveWrapper.innerHTML += `<style>${wavesStyle}</style>`;
+window.addEventListener('resize', initWaves);
