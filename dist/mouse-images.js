6d3655c7bae6baf06497c6cfb8fa13447b9580e5
medved_v_ushanke <getingjagare@gmail.com>
2020-09-18 14:24:21 +0300

    mouse images completed

--- dist/mouse-images.js
+++ dist/mouse-images.js
@@ -97,21 +97,42 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_blocks_mouse_images_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../scss/blocks/mouse-images.scss */ "./src/scss/blocks/mouse-images.scss");
 /* harmony import */ var _scss_blocks_mouse_images_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_blocks_mouse_images_scss__WEBPACK_IMPORTED_MODULE_0__);
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 
 document.addEventListener('DOMContentLoaded', function () {
   var mouseImages = document.querySelector('.mouse-images');
   var boundingRect = mouseImages.getBoundingClientRect();
   var images = Array.from(mouseImages.children);
-  var overlayWidth = mouseImages.offsetWidth / images.length;
-  var visibleIndex = 0;
-  images[visibleIndex].style.display = 'block';
-  mouseImages.addEventListener('mousemove', function (event) {
-    var x = event.clientX - boundingRect.left;
-    var imageIndex = Math.floor(x / overlayWidth);
-    images[visibleIndex].style = null;
-    images[imageIndex].style.display = 'block';
-    visibleIndex = imageIndex;
+
+  var indexes = _toConsumableArray(images.keys()).sort(function () {
+    return Math.random() - 0.5;
   });
+
+  var overlayWidth = mouseImages.offsetWidth / images.length;
+  var visibleIndex = indexes[0];
+  images[visibleIndex].style.display = 'block';
+
+  function changeImage(event) {
+    var x = (event.type === 'touchmove' ? event.targetTouches[0].clientX : event.clientX) - boundingRect.left;
+    var index = Math.floor(x / overlayWidth);
+    images[visibleIndex].style = null;
+    images[indexes[index]].style.display = 'block';
+    visibleIndex = indexes[index];
+  }
+
+  mouseImages.addEventListener('mousemove', changeImage);
+  mouseImages.addEventListener('touchmove', changeImage);
 });
 
 /***/ }),
--- src/js/blocks/mouse-images.js
+++ src/js/blocks/mouse-images.js
@@ -5,22 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const mouseImages = document.querySelector('.mouse-images');
     const boundingRect = mouseImages.getBoundingClientRect();
     const images = Array.from(mouseImages.children);
+    const indexes = [...images.keys()].sort(() => Math.random() - 0.5);
+
     const overlayWidth = mouseImages.offsetWidth / images.length;
 
-    let visibleIndex = 0;
+    let visibleIndex = indexes[0];
 
     images[visibleIndex].style.display = 'block';
 
-    mouseImages.addEventListener('mousemove', function (event) {
+    function changeImage (event) {
 
-        const x = event.clientX - boundingRect.left;
-        const imageIndex = Math.floor(x / overlayWidth);
+        const x = (event.type === 'touchmove' ? event.targetTouches[0].clientX : event.clientX) - boundingRect.left;
+        const index = Math.floor(x / overlayWidth);
 
         images[visibleIndex].style = null;
-        images[imageIndex].style.display = 'block';
-        visibleIndex = imageIndex;
+        images[indexes[index]].style.display = 'block';
+        visibleIndex = indexes[index];
+    }
 
-    });
+    mouseImages.addEventListener('mousemove',changeImage);
+    mouseImages.addEventListener('touchmove',changeImage);
 
 
 });
