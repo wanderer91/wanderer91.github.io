380ff497410c2dff379b63a03b6e805bfedc1cd1
Nikita <getingjagare@gmail.com>
2026-03-27 23:49:49 +0100

    demo removed

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import $ from "jquery";
 import axios from "axios";
 import Projects from "./index/blocks/Projects.js";
 import Tools from "./index/blocks/Tools.js";
-import Demo from "./index/blocks/Demo.js";
 import ScrollToTop from "./shared/blocks/ScrollToTop.js";
 import ScrollTriggerLink from "./shared/blocks/ScrollTriggerLink.js";
 import Navigation from "./shared/blocks/Navigation.js";
@@ -18,7 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     ScrollTriggerLink.attachEvents();
     new Projects(".portfolio__row");
     new Tools(".skills .section__content");
-    new Demo(".demo .section__content");
     new ScrollToTop();
     new Footer(".footer");
     Modal.attachEvents();
